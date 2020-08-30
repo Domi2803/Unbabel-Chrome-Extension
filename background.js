8b01dbe2f5801d2e233f4b1ecedf740284177be2
Domi2803 <domi2803@live.de>
2020-08-30 15:22:38 +0200

    Different Update Rate and Refresh on Tab Change Event
    The update rate will now lower if the user isn't active on unbabel.com:
    4 times a minute if active
    once a minute if inactive
    Also, added event hooks if user changes tab to instantly update whether the active mode.

--- background.js
+++ background.js
@@ -1,39 +1,58 @@
-setInterval(checkBalance, 10000);
-fetchCredentials().then(()=>{checkBalance();});
+// init
+fetchCredentials().then(()=>{update();});
 
+// if user changes tab -> check for current tab url
+chrome.tabs.onActiveChanged.addListener(checkTab);
+chrome.tabs.onCreated.addListener(checkTab);
+chrome.tabs.onUpdated.addListener(checkTab);
 
 var email;
 var password;
-var mode = false; // false: show tasks, true: show balance
+var isActive = false; // false: show tasks, true: show balance
 
-function checkBalance(){
-    getTabURL().then(url=>{
-        mode = url.indexOf("unbabel.com") != -1;
-    });
+var refreshTimeoutTask;
+
+const activeRefreshTimeout = 15000; // if unbabel.com is opened, refresh every 15 seconds.
+const inactiveRefreshTimeout = 60000; // if unbabel.com is closed, refresh every minute.
+
+function update(){
+    // reset timeout
+    clearTimeout(refreshTimeoutTask);
+
+    // login method - returns api_key, username and current balance
     var body = {"email": email, "password": password}
     fetch("https://mobile.unbabel.com/mapi/v1/login", {method: "post", headers: {"Content-Type": "application/json"},  body: JSON.stringify(body)})
     .then(res=>{
         
+        // only continue if api call is successful.
         if(res.status == 201)
             res.json().then((json=>{
                 
-                if(!mode)
-                fetch("https://mobile.unbabel.com/mapi/v1/available_tasks", {method: "get", headers: {"Authorization": "ApiKey " + json.username + ":" + json.api_key}}).then(res=>res.json()).then(json=>{
+                if(!isActive)   // if unbabel.com isn't the active tab, make another request using the api_key to get the current number of available tasks
+                fetch("https://mobile.unbabel.com/mapi/v1/available_tasks", {
+                    method: "get", 
+                    headers: {"Authorization": "ApiKey " + json.username + ":" + json.api_key}}) // auth-header used by api: Authorization: "ApiKey (username):(api key)"
+                .then(res=>res.json()).then(json=>{
                     
+                    // if you have multiple language pairs, sum up all of the task counts
                     var totalTasks = 0;
                     for(var i = 0; i < json.paid.length; i++){
                         totalTasks += json.paid[i].tasks_available;
                     }
+
+                    // set badgetext to total number of tasks
                     chrome.browserAction.setBadgeText({text: totalTasks.toString()});
                 })
-                else
-                chrome.browserAction.setBadgeText({text: "$"+json.balance});
+                else // isActive == true -> user is currently on the unbabel website
+                chrome.browserAction.setBadgeText({text: "$"+json.balance}); // set badgetext to current balance
             }))
-        else
-        chrome.browserAction.setBadgeText({text: ""});
+        else // api call unsuccessful
+        chrome.browserAction.setBadgeText({text: ""}); 
     })
 
-    fetchCredentials();
+    // depending on whether current tab is unbabel or not, update again in x seconds.
+    const timeout = isActive ? activeRefreshTimeout : inactiveRefreshTimeout;
+    refreshTimeoutTask = setTimeout(update, timeout);
 }
 
 function fetchCredentials(){
@@ -47,13 +66,23 @@ function fetchCredentials(){
     })
 }
 
-function getTabURL(){
+// called on event hook, if user changes tab
+function checkTab(){
+    getActiveTabURL().then(url=>{
+        var newIsActive = url.indexOf("unbabel.com") != -1; 
+        if(newIsActive != isActive)
+            update();
+        isActive = newIsActive;
+    });
+}
+
+// returns url of currently active tab
+function getActiveTabURL(){
     return new Promise((res, rej)=>{
         chrome.tabs.query({
             active: true,
             lastFocusedWindow: true
         }, function(tabs) {
-            // and use that tab to fill in out title and url
             var tab = tabs[0];
             if(tab)
             res(tab.url);
